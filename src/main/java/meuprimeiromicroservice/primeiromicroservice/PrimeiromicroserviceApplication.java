b44e08e725497453c618b272be52fa5929f0692f
Luis Fellipe <luis.fellipe1217@gmail.com>
2020-10-14 10:07:17 -0300

    Iniciando projeto Spring

---
+++ src/main/java/meuprimeiromicroservice/primeiromicroservice/PrimeiromicroserviceApplication.java
@@ -0,0 +1,13 @@
+package meuprimeiromicroservice.primeiromicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeiromicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeiromicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/meuprimeiromicroservice/primeiromicroservice/PrimeiromicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package meuprimeiromicroservice.primeiromicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiromicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
